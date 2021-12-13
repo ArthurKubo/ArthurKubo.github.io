8bc4c1db87bcc8f2fb1799c0ab63fc7ecdc16860
ArthurKubo <95389077+ArthurKubo@users.noreply.github.com>
2021-12-12 23:14:10 -0300

    Estudo - Header

---
+++ Header.js
@@ -0,0 +1,13 @@
+// Estudo - Usando push para enviar valores ao dataLayer
+var phone = document.querySelector(".mdl-navigation__link mdl-typography--text-uppercase");
+window.dataLayer= window.dataLayer || [];
+dataLayer.addEventListener('click', function(){
+    dataLayer.push({
+        'event':'generic:event',
+        'eventCategory':'android:header',
+        'eventAction': 'click:header',
+        'eventLabel':'phone'
+
+        console.log("Phone colocado no dataLayer")
+    })
+})
