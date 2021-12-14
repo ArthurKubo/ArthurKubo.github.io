272dc4114efa727848a3f98b28fb57402d4a6063
ArthurKubo <95389077+ArthurKubo@users.noreply.github.com>
2021-12-14 12:54:41 -0300

    blablabla

--- headerteste.js
+++ headerteste.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 var play = document.querySelectorAll(".mdl-navigation__link mdl-typography--text-uppercase")[0];
-console.log(play);
-=======
-var play = document.querySelectorAll(".mdl-navigation__link mdl-typography--text-uppercase")[6];
 console.log(play)
 console.log("o console leu até essa parte");
->>>>>>> 79bc19e936163db3a74166cc9fad20334ec14e92
 play.addEventListener('click', function() {
   window.dataLayer= window.dataLayer || [];
   window.dataLayer.push({
