54f9822ecb4543e8142f4c4f49ddcf2b36d739cf
ArthurKubo <95389077+ArthurKubo@users.noreply.github.com>
2022-01-07 19:20:18 -0300

    android:footer

---
+++ header.js
@@ -0,0 +1,11 @@
+var phone = document.querySelectorAll(".android-navigation mdl-navigation")[0];
+phone.addEventListener('click', function(){
+    window.dataLayer= window.dataLayer || [];
+    window.dataLayer.push({
+        'event': 'generic:event',
+        'eventCategory':'header',
+        'eventAction':'click:header',
+        'eventLabel':'phone'
+    });
+    console.log("phone push to dataLayer");
+});
