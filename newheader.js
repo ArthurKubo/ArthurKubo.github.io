e7fb7c688ecc5dd625cbdd328c30032bec852d04
ArthurKubo <95389077+ArthurKubo@users.noreply.github.com>
2022-01-17 12:53:49 -0300

    click

--- newheader.js
+++ newheader.js
@@ -2,34 +2,34 @@
 
 //PHONE
 var phone = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[0];
-phone.addEventListener('click', pushinfo('generic:event', 'android:header', 'click:img', 'phones'))
+phone.addEventListener("click", pushinfo('generic:event', 'android:header', 'click:img', 'phones'))
 // TABLETS
 var tablets = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[1];
-tablets.addEventListener('click', pushinfo('generic:event', 'android:header', 'click:img', 'tablets'));
+tablets.addEventListener("click", pushinfo('generic:event', 'android:header', 'click:img', 'tablets'));
 
 //WEAR
 var wear = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[2];
-wear.addEventListener('click', pushinfo('generic:event', 'android:header', 'click:img', 'wear'));
+wear.addEventListener("click", pushinfo('generic:event', 'android:header', 'click:img', 'wear'));
 
 //TV
 var tv = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[3];
-tv.addEventListener('click', pushinfo('generic:event', 'android:header', 'click:img', 'TV'));
+tv.addEventListener("click", pushinfo('generic:event', 'android:header', 'click:img', 'TV'));
 
 //AUTO
 var auto = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[4];
-auto.addEventListener('click', pushinfo('generic:event', 'android:header', 'click:img', 'auto'));
+auto.addEventListener("click", pushinfo('generic:event', 'android:header', 'click:img', 'auto'));
 //ONE
 var one = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[5];
-one.addEventListener('click', pushinfo('generic:event', 'android:header', 'click:img', 'one'));
+one.addEventListener("click", pushinfo('generic:event', 'android:header', 'click:img', 'one'));
 //PLAY
 var play = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[6];
-play.addEventListener('click', pushinfo('generic:event', 'android:header', 'click:img', 'play'));
+play.addEventListener("click", pushinfo('generic:event', 'android:header', 'click:img', 'play'));
 //SEARCH ICON
 var search = document.querySelector(".mdl-button.mdl-js-button.mdl-button--icon");
-search.addEventListener('click', pushinfo('generic:event', 'android:header', 'click:img', 'search'));
+search.addEventListener("click", pushinfo('generic:event', 'android:header', 'click:img', 'search'));
 //MORE BUTTON
 var more = document.querySelector("#more-button");
-more.addEventListener('click', pushinfo('generic:event', 'android:header', 'click:img', 'more'));
+more.addEventListener("click", pushinfo('generic:event', 'android:header', 'click:img', 'more'));
 
 //FUNÇÃO PUSH
 function pushinfo (event, category, action, label){
