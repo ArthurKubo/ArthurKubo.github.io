bed36687708f2c52e192c1181a7f9443eba495d0
ArthurKubo <95389077+ArthurKubo@users.noreply.github.com>
2022-01-17 16:12:46 -0300

    codigo limpo

--- newheader.js
+++ newheader.js
@@ -5,38 +5,28 @@ var phone = document.querySelectorAll(".mdl-navigation__link.mdl-typography--tex
 phone.addEventListener("click", function(){pushinfo('generic:event', 'android:header', 'click:img', 'phones')});
 // TABLETS
 var tablets = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[1];
-addEventListener("click", function(){pushinfo('generic:event', 'android:header', 'click:img', 'phones')});
-
+addEventListener("click", function(){pushinfo('generic:event', 'android:header', 'click:img', 'tablets')});
 //WEAR
 var wear = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[2];
-var event = ['generic:event', 'android:header', 'click:img', 'wear']
-wear.addEventListener("click", pushinfo('generic:event', 'android:header', 'click:img', 'wear'));
-
+addEventListener("click", function(){pushinfo('generic:event', 'android:header', 'click:img', 'wear')});
 //TV
 var tv = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[3];
-var event = ['generic:event', 'android:header', 'click:img', 'tv']
-tv.addEventListener("click", pushinfo);
+tv.addEventListener("click", function(){pushinfo('generic:event', 'android:header', 'click:img', 'tv')});
 //AUTO
 var auto = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[4];
-var event = ['generic:event', 'android:header', 'click:img', 'auto']
-auto.addEventListener("click", pushinfo);
+auto.addEventListener("click", function(){pushinfo('generic:event', 'android:header', 'click:img', 'auto')});
 //ONE
 var one = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[5];
-var event = ['generic:event', 'android:header', 'click:img', 'one']
-one.addEventListener("click", pushinfo);
+one.addEventListener("click", function(){pushinfo('generic:event', 'android:header', 'click:img', 'one')});
 //PLAY
 var play = document.querySelectorAll(".mdl-navigation__link.mdl-typography--text-uppercase")[6];
-var event = ['generic:event', 'android:header', 'click:img', 'play']
-play.addEventListener("click", pushinfo);
+play.addEventListener("click", function(){pushinfo('generic:event', 'android:header', 'click:img', 'play')});
 //SEARCH ICON
 var search = document.querySelector(".mdl-button.mdl-js-button.mdl-button--icon");
-var event = ['generic:event', 'android:header', 'click:img', 'search']
-search.addEventListener("click", pushinfo);
+search.addEventListener("click", function(){pushinfo('generic:event', 'android:header', 'click:img', 'search')});
 //MORE BUTTON
 var more = document.querySelector("#more-button");
-var event = ['generic:event', 'android:header', 'click:img', 'more']
-more.addEventListener("click", pushinfo);
-
+more.addEventListener("click", function(){pushinfo('generic:event', 'android:header', 'click:img', 'more')});
 //FUNÇÃO PUSH
 function pushinfo (event, category, action, label){
     window.dataLayer= window.dataLayer || [];
